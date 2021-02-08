5c5b15b59153fff2783aee5724b71513a708d559
simonxue <xueyao.me@gmail.com>
2021-02-08 20:50:13 +0800

    feat-用户登陆

--- src/test/java/me/xueyao/SimonFastBlogApiApplicationTests.java
+++ src/test/java/me/xueyao/SimonFastBlogApiApplicationTests.java
@@ -1,13 +1,22 @@
 package me.xueyao;
 
+import me.xueyao.entity.User;
+import me.xueyao.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimonFastBlogApiApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
     @Test
-    void contextLoads() {
+    void testCreate() {
+        User user = new User().setNickname("xueyao")
+                .setUsername("xueyao").setPassword("simon123");
+        user.setCreator("admin");
+        userRepository.save(user);
     }
 
 }
